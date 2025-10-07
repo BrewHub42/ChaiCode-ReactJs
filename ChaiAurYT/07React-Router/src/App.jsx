86ebe4d7d1120a0c8bc678661bbd1a9048834109
Sushant Shinde <98010962+Maverick341@users.noreply.github.com>
2025-10-08 00:31:11 +0530

    feat: Initialize React Router application with basic structure and components
    - Added main application file (App.jsx) with routing setup using react-router-dom.

---
+++ ChaiAurYT/07React-Router/src/App.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Github, { githubInfoLoader } from "./components/Github/Github";
+import User from "./components/User/User";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "github",
+        element: <Github />,
+        loader: () =>  githubInfoLoader()
+      },
+      {
+        path: "user/:userid",
+        element: <User />,
+      },
+    ],
+  },
+]);
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<Layout />}>
+//       <Route path="" element={<Home />} />
+//       <Route path="about" element={<About />} />
+//       <Route path="contact" element={<Contact />} />
+//       <Route
+//         loader={githubInfoLoader}
+//         path="github"
+//         element={<Github />}
+//       />
+//       <Route path="user/:userid" element={<User />} />
+//     </Route>
+//   )
+// );
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
